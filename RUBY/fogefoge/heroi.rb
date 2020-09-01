23055cbf311adc1733588f8a1874e884f91c151a
Marcelle Herescu <maherescu@gmail.com>
2020-09-01 15:59:08 -0300

    adicionado descrição da classe no arquivo

--- RUBY/fogefoge/heroi.rb
+++ RUBY/fogefoge/heroi.rb
@@ -1,5 +1,5 @@
 class Heroi
-    attr_accessor :linha, :coluna
+    attr_accessor :linha, :coluna #o método, função attr_accessor usa os símbolos linha e coluna (linha e coluna não são variáveis nem funções, são símbolos)
 end
 
 # marcelle = Heroi.new
@@ -7,7 +7,7 @@ end
 # marcelle.coluna = 3
 
 # puts "A Marcelle está em #{marcelle.linha} #{marcelle.coluna}"
-# puts marcelle #aqui ele vai imprimir o lugar da memória que está este herói nomeado como Marcelle
+# puts marcelle #aqui ele vai imprimir o hash, o lugar da memória que está este herói nomeado como Marcelle
 
 # vinicius = Heroi.new
 # vinicius.linha = 6
